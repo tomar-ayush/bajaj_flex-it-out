65bd372e13f3455ad20dd8593ca37483f63b448f
Ayush <ayushtomar8624@gmail.com>
2025-02-15 01:34:12 +0530

    feat: created otp sending and verification routes

---
+++ app/api/auth/send-otp/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+import nodemailer from "nodemailer"
+import Otp from "@/models/otpModel";
+import connectDB from "@/utils/db";
+
+export async function POST(req: NextRequest) {
+	const { email } = await req.json();
+	const otp = Math.floor(100000 + Math.random() * 900000).toString();
+
+	connectDB();
+	const otp_obj = new Otp({ email, otp, createdAt: Date.now() });
+	await otp_obj.save();
+
+	const transporter = nodemailer.createTransport({
+		host: process.env.EMAIL_HOST,
+		port: parseInt(process.env.EMAIL_PORT!, 10),
+		secure: true,
+		auth: {
+			user: process.env.EMAIL_USER,
+			pass: process.env.EMAIL_PASS,
+		},
+	});
+
+	await transporter.sendMail({
+		from: process.env.EMAIL_USER,
+		to: email,
+		subject: "Your OTP",
+		text: `Your OTP is ${otp}`,
+	});
+
+
+	return NextResponse.json({ message: "OTP sent successfully" });
+}
---
+++ app/api/auth/verify-otp/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import Otp from "../../../../models/otpModel";
+
+export async function POST(req: Response) {
+	const { email, otp } = await req.json();
+
+	// Fetch the latest OTP from the database for the given email
+	const storedOtp = await Otp.find({ email }).sort({ createdAt: -1 }).limit(1);
+	console.log(storedOtp);
+
+	// If no OTP is found or OTP has expired
+	if (!storedOtp) {
+		return NextResponse.json(
+			{ error: "OTP expired or invalid" },
+			{ status: 400 }
+		);
+	}
+
+	// Compare the OTP with the one sent
+	if (storedOtp[0].otp !== otp) {
+		return NextResponse.json({ error: "Incorrect OTP" }, { status: 400 });
+	}
+
+	return NextResponse.json(
+		{ message: "OTP verified successfully" },
+		{ status: 200 }
+	);
+}
+
