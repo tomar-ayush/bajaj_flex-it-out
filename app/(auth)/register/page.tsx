782c60c9124279e16990fca3b134cd186c5379f0
Aryan Inguz <117257768+Aryainguz@users.noreply.github.com>
2025-02-14 23:46:15 +0530

    feat: added registration page with form and image integration

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { RegisterForm } from "@/components/auth/register/register-form";
+import registerImage from "@/public/login-image.avif"; // Replace with registerImage if available
+import logo from "@/public/logo.png";
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
 
-const Register = () => {
+export const metadata: Metadata = {
+  title: "Flex It Out | Register",
+  description: "Flex It Out",
+};
+
+export default function RegisterPage() {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="grid min-h-svh lg:grid-cols-2">
+      {/* Image Column (now on the left) */}
+      <div className="relative hidden bg-muted lg:block">
+        <Image
+          src={registerImage}
+          alt="Register Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
 
-export default Register
+      {/* Form Column (now on the right) */}
+      <div className="flex flex-col px-6 py-16 sm:mt-0 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <Link
+            href="/"
+            className="flex rounded outline-none font-bold text-3xl focus:ring-gray-900 focus:ring-offset-2"
+          >
+            <Image
+              src={logo}
+              alt="logo"
+              width={55}
+              height={55}
+              className="relative bottom-2 mr-2"
+            />
+          </Link>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <RegisterForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
