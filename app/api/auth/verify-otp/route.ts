75a407041399617562a46b7b43a7f553bf120fe6
Aryan Inguz <117257768+Aryainguz@users.noreply.github.com>
2025-02-16 04:39:20 +0530

    fix: enhance OTP verification logic and improve error handling

--- app/api/auth/verify-otp/route.ts
+++ app/api/auth/verify-otp/route.ts
@@ -1,27 +1,44 @@
-import { NextResponse, NextRequest } from "next/server";
-import Otp from "../../../../models/otpModel";
+import Otp from "@/models/otpModel";
+import connectDB from "@/utils/db";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-  const { email, otp } = await req.json();
+  try {
+    await connectDB();
+    const { email, otp } = await req.json();
 
-  const storedOtp = await Otp.find({ email }).sort({ createdAt: -1 }).limit(1);
+    const storedOtp = await Otp.findOne({ email }).sort({ createdAt: -1 });
 
-  if (!storedOtp || storedOtp.length === 0) {
+    if (!storedOtp) {
+      return NextResponse.json(
+        { error: "OTP expired or invalid", valid: false },
+        { status: 400 }
+      );
+    }
+
+    if (storedOtp.expiresAt < Date.now()) {
+      return NextResponse.json(
+        { error: "OTP has expired", valid: false },
+        { status: 400 }
+      );
+    }
+
+    if (storedOtp.otp !== otp) {
+      return NextResponse.json(
+        { error: "Incorrect OTP", valid: false },
+        { status: 400 }
+      );
+    }
+
     return NextResponse.json(
-      { error: "OTP expired or invalid", valid: false },
-      { status: 400 }
+      { message: "OTP verified successfully", valid: true },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error verifying OTP:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error", valid: false },
+      { status: 500 }
     );
   }
-
-  if (storedOtp[0].otp !== otp) {
-    return NextResponse.json(
-      { error: "Incorrect OTP", valid: false },
-      { status: 400 }
-    );
-  }
-
-  return NextResponse.json(
-    { message: "OTP verified successfully", valid: true },
-    { status: 200 }
-  );
 }
