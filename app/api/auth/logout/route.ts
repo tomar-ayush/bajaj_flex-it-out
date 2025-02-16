b8cde3b1aa99abd99bd9b2ccdeb2bd6a358b38dc
Ayush <ayushtomar8624@gmail.com>
2025-02-16 16:56:15 +0530

    bug : update logout route

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -10,11 +10,17 @@ export async function GET(request: NextRequest) {
 	const cookieOptions = {
 		expires: new Date(0),
 		path: "/",
-		secure: process.env.NODE_ENV === "production", // Secure in production only
+		// secure: process.env.NODE_ENV === "production", // Secure in production only
 		sameSite: "lax" as const,
 		// Don't set domain for localhost
-		...(domain !== "localhost" && { domain: `.${domain}` })
+		// ...(domain !== "localhost" && { domain: `.${domain}` })
 	};
+
+	response.cookies.set("__Secure-next-auth.session-token", "", cookieOptions);
+	response.cookies.set("__Host-next-auth.csrf-token", "", cookieOptions);
+	response.cookies.set("__Secure-next-auth.callback-url", "", cookieOptions);
+	response.cookies.set("__Secure-next-auth.pkce.code_verifier", "", cookieOptions);
+	response.cookies.set("__Secure-next-auth.state", "", cookieOptions);
 
 
 	const preserveCookies = new Set([
@@ -26,6 +32,7 @@ export async function GET(request: NextRequest) {
 	// Clear all cookies except those in preserveCookies
 	request.cookies.getAll().forEach(cookie => {
 		if (!preserveCookies.has(cookie.name)) {
+			console.log(cookie);
 			response.cookies.set(cookie.name, "", cookieOptions);
 		}
 	});
